f011c30a8f851a993b447888404be84d330e6b9d
Elioth <dharsuthdra@gmail.com>
2025-03-21 15:41:06 +0700

    Fixing Login Screen

--- lib/presentation/pages/auth/login_screen.dart
+++ lib/presentation/pages/auth/login_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kawan_tani/presentation/pages/dashboard/home_screen.dart';
 import 'package:flutter_kawan_tani/presentation/pages/auth/signup_screen.dart';
+import 'package:flutter_kawan_tani/shared/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:get/get.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
@@ -11,162 +14,256 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool obscureText = true;
 
-  Widget buildTextField(
-      IconData icon, String hint, TextEditingController controller) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.grey[200],
-            prefixIcon: Icon(icon, color: Colors.grey),
-            hintText: hint,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide.none,
-            ),
-          ),
-        ),
-        const SizedBox(height: 15),
-      ],
-    );
-  }
-
-  void handleNext() {
-    if (emailController.text.isEmpty || passwordController.text.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text("Harap isi semua data sebelum melanjutkan."),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return;
-    }
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const HomeScreen()),
-    );
-  }
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      backgroundColor: const Color(0xFF78D14D),
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          // Header
-          Container(
-            padding: const EdgeInsets.only(top: 70, bottom: 30),
-            child: Column(
-              children: [
-                Text(
-                  "Kawan Tani",
-                  style: GoogleFonts.poppins(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  "Teman Bertani Anda",
-                  style: GoogleFonts.poppins(fontSize: 16, color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-
-          // Form dalam Expanded agar tombol tetap di bawah
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(30),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
-                ),
-              ),
+        backgroundColor: const Color(0xFF78D14D),
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 30.0) +
+                  EdgeInsets.symmetric(horizontal: 16.0) +
+                  EdgeInsets.symmetric(vertical: 54.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Daftar Akun",
-                    style: GoogleFonts.poppins(
-                        fontSize: 22, fontWeight: FontWeight.bold),
+                  Text("KawanTani",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          fontSize: 36.0, fontWeight: bold, color: whiteColor)),
+                  SizedBox(
+                    height: 5.0,
                   ),
-                  const SizedBox(height: 5),
-                  Text("Daftar sekarang dan nikmati berbagai\nfitur menarik!",
-                      style: GoogleFonts.poppins(fontSize: 14)),
-
-                  const SizedBox(height: 20),
-
-                  buildTextField(
-                      Icons.email, "johndoe@examplemail.com", emailController),
-                  buildTextField(
-                      Icons.lock, "****************", passwordController),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: handleNext,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF78D14D),
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text("Masuk",
-                        style: GoogleFonts.poppins(
-                            color: Colors.white, fontSize: 16)),
-                  ),
-
-                  const SizedBox(height: 250),
-
-                  // Tombol di bagian bawah
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                      children: [
-                        Text(
-                          "Belum memiliki akun?",
-                          style: GoogleFonts.poppins(fontSize: 14),
-                        ),
-                        const SizedBox(height: 10),
-                        OutlinedButton(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        const SignUpScreen()));
-                          },
-                          style: OutlinedButton.styleFrom(
-                            side: const BorderSide(color: Color(0xFF78D14D)),
-                            minimumSize: const Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          child: Text("Daftar",
-                              style: GoogleFonts.poppins(
-                                  color: const Color(0xFF78D14D),
-                                  fontSize: 16)),
-                        ),
-                      ],
-                    ),
-                  ),
+                  Text("Teman Bertani Anda",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                          fontSize: 18.0,
+                          fontWeight: light,
+                          color: whiteColor)),
                 ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 28.0) +
+                    EdgeInsets.symmetric(horizontal: 38.0),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: whiteColor, // Set background color here
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0), // Set border radius
+                    topRight: Radius.circular(30.0),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                "Masuk",
+                                textAlign: TextAlign.left,
+                                style: GoogleFonts.poppins(
+                                    fontSize: 28.0,
+                                    color: blackColor,
+                                    fontWeight: semiBold),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Container(
+                                width: MediaQuery.of(context).size.width * 0.77,
+                                child: Text(
+                                  "Masuk ke akun anda, untuk melanjutkan",
+                                  textAlign: TextAlign.left,
+                                  style: GoogleFonts.poppins(
+                                      fontSize: 15.0,
+                                      color: blackColor,
+                                      fontWeight: light),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 16.0),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Email",
+                                    textAlign: TextAlign.left,
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 15, color: blackColor),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            8.0), // Single horizontal padding
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffE7EFF2),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: TextFormField(
+                                      controller: _emailController,
+                                      keyboardType: TextInputType.emailAddress,
+                                      decoration: InputDecoration(
+                                        hintText: "johndoe@examplemail.com",
+                                        hintStyle: GoogleFonts.poppins(
+                                            fontSize: 15.0, fontWeight: light),
+                                        prefixIcon: PhosphorIcon(
+                                          PhosphorIconsRegular.envelope,
+                                          size: 19.0,
+                                          color: Color(0xff8594AC),
+                                        ),
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.symmetric(
+                                            vertical: 10.0),
+
+                                        // Adjust vertical padding
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Password",
+                                    textAlign: TextAlign.left,
+                                    style: GoogleFonts.poppins(
+                                        fontSize: 15, color: blackColor),
+                                  ),
+                                  SizedBox(height: 8.0),
+                                  Container(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal:
+                                            8.0), // Single horizontal padding
+                                    decoration: BoxDecoration(
+                                      color: Color(0xffE7EFF2),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    child: TextFormField(
+                                      controller: _passwordController,
+                                      obscureText: obscureText,
+                                      decoration: InputDecoration(
+                                        hintText: "*********",
+                                        hintStyle: GoogleFonts.poppins(
+                                            fontSize: 15.0, fontWeight: light),
+                                        prefixIcon: PhosphorIcon(
+                                          PhosphorIconsRegular.lock,
+                                          size: 19.0,
+                                          color: Color(0xff8594AC),
+                                        ),
+                                        suffixIcon: GestureDetector(
+                                          onTap: () {
+                                            setState(() {
+                                              obscureText = !obscureText;
+                                            });
+                                          },
+                                          child: PhosphorIcon(
+                                              obscureText
+                                                  ? PhosphorIconsRegular
+                                                      .eyeSlash
+                                                  : PhosphorIconsRegular.eye,
+                                              size: 19.0,
+                                              color: Color(0xff8594AC)),
+                                        ),
+                                        border: InputBorder.none,
+                                        contentPadding: EdgeInsets.symmetric(
+                                            vertical: 10.0),
+
+                                        // Adjust vertical padding
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(height: 20.0),
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    print('Email: ${_emailController.text}');
+                                    print(
+                                        'Password: ${_passwordController.text}');
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: primaryColor,
+                                  elevation: 0.0,
+                                  shadowColor: Colors.transparent,
+                                  minimumSize: Size(double.infinity, 48),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                child: Text(
+                                  'Masuk',
+                                  style: GoogleFonts.poppins(
+                                      color: whiteColor,
+                                      fontSize: 16,
+                                      fontWeight: bold),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Belum memiliki akun?",
+                          style: GoogleFonts.poppins(
+                              fontSize: 14, fontWeight: light),
+                        ),
+                        SizedBox(height: 15.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            Get.to(() => SignUpScreen());
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: whiteColor,
+                              elevation: 0.0,
+                              shadowColor: Colors.transparent,
+                              minimumSize: Size(double.infinity, 48),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              side: BorderSide(color: primaryColor)),
+                          child: Text(
+                            'Daftar Akun',
+                            style: GoogleFonts.poppins(
+                                color: primaryColor,
+                                fontSize: 16,
+                                fontWeight: bold),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
