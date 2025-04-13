f0d0842626ffdb29d53b8aba606e1ff2ae707c00
Pilemon <barimbingpilemon@gmail.com>
2025-04-13 22:10:42 +0700

    register-workshop#1

---
+++ lib/presentation/pages/workshops/register_workshop.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterWorkshop extends StatefulWidget {
+  const RegisterWorkshop({super.key});
+
+  @override
+  State<RegisterWorkshop> createState() => _RegisterWorkshopState();
+}
+
+class _RegisterWorkshopState extends State<RegisterWorkshop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
